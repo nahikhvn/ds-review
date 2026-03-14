e02240a373a8c19248c32173263aa36c81dde3e2
nahikhvn <nahikhan@proton.me>
2026-03-14 18:54:42 -0400

    math review done

---
+++ python_review/math.py
@@ -0,0 +1,19 @@
+x, y = 10, 20
+
+print(x + y)
+print(x - y)
+print(x * y)
+print(x / y)
+
+print( x // y)
+print(x % y)
+print(x ** y)
+
+# logical operators
+truth, fake = True, False
+print(truth or fake)
+print(truth and fake)
+print(not truth)
+print(not fake)
+
+# the order is as follows: paren, not, and, then or
